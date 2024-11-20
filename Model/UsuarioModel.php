702f45d41cf99e3ebe503956a92fb66f9cd85f99
Adri Lopez <adrielopezp@gmail.com>
2024-11-20 17:29:22 -0600

    usuarioModel.php agregado

---
+++ Model/UsuarioModel.php
@@ -0,0 +1,94 @@
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/Model/BaseDatos.php';
+
+    function ConsultarUsuarioModel($consecutivo)
+    {
+        try
+        {
+            $enlace = AbrirBD();
+
+            $sentencia = "CALL ConsultarUsuario('$consecutivo')";
+            $resultado = $enlace -> query($sentencia);
+
+            CerrarBD($enlace);
+            return $resultado;
+        }
+        catch(Exception $ex)
+        {
+            return null;
+        }
+    }
+
+    function ConsultarUsuariosModel($consecutivo)
+    {
+        try
+        {
+            $enlace = AbrirBD();
+
+            $sentencia = "CALL ConsultarUsuarios('$consecutivo')";
+            $resultado = $enlace -> query($sentencia);
+
+            CerrarBD($enlace);
+            return $resultado;
+        }
+        catch(Exception $ex)
+        {
+            return null;
+        }
+    }
+
+    function ActualizarPerfilModel($consecutivo,$identificacion,$nombre,$correo,$rol)
+    {
+        try
+        {
+            $enlace = AbrirBD();
+
+            $sentencia = "CALL ActualizarPerfil('$consecutivo','$identificacion','$nombre','$correo','$rol')";
+            $resultado = $enlace -> query($sentencia);
+
+            CerrarBD($enlace);
+            return $resultado;
+        }
+        catch(Exception $ex)
+        {
+            return false;
+        }
+    }
+
+    function CambiarEstadoUsuarioModel($consecutivo)
+    {
+        try
+        {
+            $enlace = AbrirBD();
+
+            $sentencia = "CALL CambiarEstadoUsuario('$consecutivo')";
+            $resultado = $enlace -> query($sentencia);
+
+            CerrarBD($enlace);
+            return $resultado;
+        }
+        catch(Exception $ex)
+        {
+            return false;
+        }
+    }
+
+    function ConsultarRolesModel()
+    {
+        try
+        {
+            $enlace = AbrirBD();
+
+            $sentencia = "CALL ConsultarRoles()";
+            $resultado = $enlace -> query($sentencia);
+
+            CerrarBD($enlace);
+            return $resultado;
+        }
+        catch(Exception $ex)
+        {
+            return null;
+        }
+    }
+
+?>
